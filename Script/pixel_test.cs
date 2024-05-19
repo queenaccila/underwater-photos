3f950ff019e169555082c4b1b664e8438d7336c4
Helen Ho <helenho2k1@gmail.com>
2024-05-19 18:32:05 +0900

    Changed cam ui to save time + simplicity
    Is now just full screen so I can save time instead of cropping an image

--- Script/pixel_test.cs
+++ Script/pixel_test.cs
@@ -4,7 +4,13 @@ using System;
 public partial class pixel_test : Node3D
 {
 	private CanvasLayer _photoMode;
+	private TextureRect _screenshotDisplay;
+	private Viewport _viewport;
 	bool photo = false;
+	private int screenshotCount = 0;
+	
+	[Signal]
+	public delegate void PhotoTakenEventHandler();
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -12,11 +18,15 @@ public partial class pixel_test : Node3D
 		_photoMode = GetNode<CanvasLayer>("PixelContainer/PhotoMode");
 		_photoMode.Visible = false;
 		photo = false;
+		
+		_screenshotDisplay = GetNode<TextureRect>("CanvasLayer/PhotoMode/ScreenshotDisplay");
+		_viewport = GetNode<Viewport>("PixelContainer/PixelViewport");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		//pulls up camera mode
 		if(Input.IsActionJustPressed("camera_mode") && photo == false)
 		{
 			_photoMode.Visible = true;
@@ -27,5 +37,31 @@ public partial class pixel_test : Node3D
 			_photoMode.Visible = false;
 			photo = false;
 		}
+		
+		//takes a photo
+		if(Input.IsActionJustPressed("take_photo") && photo == true)
+		{
+			//GD.Print("took photo");
+			TakeScreenshot();
+		}
+	}
+
+	private void TakeScreenshot()
+	{
+		// Capture the image from the viewport
+		Image img = _viewport.GetTexture().GetImage();
+		
+		// Specify the save path with file name and extension
+		var filePath = "res://underwater-photos/Screenshots/screenshot.png";
+		
+		// Save the image as a PNG
+		img.SavePng(filePath);
+
+		EmitSignal(SignalName.PhotoTaken);
+	}
+	
+	private void _on_photo_taken()
+	{
+		GD.Print("photo");
 	}
 }
